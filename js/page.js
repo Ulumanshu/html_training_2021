5d4fab38a6dd663aff51080e37e066b501f58661
Ulumanshu <karolis_kesminas@yahoo.com>
2021-05-16 09:43:58 +0300

    Add training project data

---
+++ js/page.js
@@ -0,0 +1,11 @@
+function paslepk_raudona() {
+    var element = document.getElementById("xx");
+    console.log("MUSU ELEMENTAS: ", element, element.classList, element.classList.contains('hidden'));
+    if (element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+        element.classList.add('shown');
+    } else {
+        element.classList.remove('shown');
+        element.classList.add('hidden');
+    }
+};
