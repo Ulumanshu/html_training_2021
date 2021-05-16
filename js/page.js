8896e018a9560fbfe7d7d72f75e6a0fe6a111cdd
Ulumanshu <karolis_kesminas@yahoo.com>
2021-05-16 13:23:11 +0300

    Add Js variable examples 10

--- js/page.js
+++ js/page.js
@@ -16,6 +16,7 @@ function paslepk_raudona() {
 };
 // JS KINTAMIEJI #####################################################
 // SIMPLE TYPES
+
 //NUMBERS
 var num_integer = 4;
 var num_float = 4.56;
@@ -23,6 +24,26 @@ var num_float = 4.56;
 var str_1 = "I am string";
 // Booleans true - false
 var blool_1 = false;
+
+// Complex types
+
+// Array sarasas
+var array_var = Array(); // var array_var = [];
+// Array push metodas prideda objektus i sarasa
+var sar_ilgis = array_var.push("Kiwi2", "Kiwi3", '4', 14, {'vardas': 'Petras'});
+console.log("Array", array_var, 'Saraso Ilgis', sar_ilgis, typeof array_var)
+// Objektas - dictionary kitose kalbose
+var dict_var = new Object();
+var dict_var2 = {};
+dict_var['type'] = 'Very Complex';
+console.log("Object", dict_var, typeof dict_var);
+console.log("Object2", dict_var2, typeof dict_var2);
+
+//Special types
+
+// null - no value ,
+// NaN - Not A Number - invalid number value,
+// undefined - when a variable that has not been declared or initialized.
 
 //########################################################
 
@@ -36,7 +57,7 @@ var d = ['a', 12, 'c'];
 var e = 5;
 var client_list = Array()
 client_list.push("Kiwi");
-var sar_ilgis = client_list.push("Kiwi2", "Kiwi3", '4', 14, {'vardas': 'Petras'});
+
 var int_num = parseInt(str_num, 10)
 var sum_1 = a + int_num;
 var sum_2 = a + str_num;
