d1af0fe57d6fbde458fae18c3fb6819f550fa197
LAPTOP-PE9B9K29\EvC <echmylko@gmail.com>
2021-05-16 09:58:34 +0300

    removed_personal_html

--- script.js
+++
@@ -1,5 +0,0 @@
-function paslepti_spon(){
-            var element = document.getElementById("spon");
-            console.log("Element", element);
-            element.classList.add ('hidden');
-        }
