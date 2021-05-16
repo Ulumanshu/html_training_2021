216614579dedc260a17b0ee9f12f46b03f114e70
Ulumanshu <karolis_kesminas@yahoo.com>
2021-05-16 12:29:16 +0300

    Add Js variable examples 4

--- js/page.js
+++ js/page.js
@@ -20,10 +20,13 @@ let b = 'TEKSTINIS KINTAMASIS b';
 const c = 19;
 var d = ['a', 12, 'c'];
 var e = 5;
+var client_list = Array()
+client_list.push("Kiwi");
+client_list.push("Kiwi2");
 
 // document.write('new text');
 
 // alert('hello');
 
 // Log if js successfully imported
-console.log('JS FUNCTIONS LOADED SUCCESFULLY!', a, b, c, d, e);
+console.log('JS FUNCTIONS LOADED SUCCESFULLY!', a, b, c, d, e, client_list);
