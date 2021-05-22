77c0531ea804f22b058e8b1ace7920f7a7c1f653
Ulumanshu <karolis_kesminas@yahoo.com>
2021-05-22 11:29:13 +0300

    Conflict new a*a karolis

--- js/page.js
+++ js/page.js
@@ -150,3 +150,9 @@ console.log("MES GAVOME", vardas);
 function confict_function(a) {
     return a * a
 };
+function confict_functionkarolis(a) {
+    return a * a
+};
+function confict_functionkarolis(a) {
+    return a * a
+};
